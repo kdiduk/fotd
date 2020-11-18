cec29484d3a0ffd3948d031c7360f71b15d0c1c3
kyryll.diduk <kyryll.diduk@dynatrace.com>
2020-11-19 00:08:27 +0100

    Cosmetic

--- src/generator.c
+++ src/generator.c
@@ -12,7 +12,7 @@ void generate_forest(uint8_t map[FOREST_HEIGHT][FOREST_WIDTH])
     for (uint8_t y = 0; y < FOREST_HEIGHT; ++y) {
 	for (uint8_t x = 0; x < FOREST_WIDTH; ++x) {
 	    if (rand() < RAND_VALUE) {
-	    map[y][x] = tile_ground;
+                 map[y][x] = tile_ground;
 	    } else {
 		map[y][x] = tile_tree;
 	    }
